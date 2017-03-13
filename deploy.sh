9fa7015b7a1d5022c392b76176210f39d65b785f
Erez Rabih <erez.rabih@gmail.com>
2017-03-13 16:28:03 +0200

    changed default value

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export RABBITMQ_REPLICAS=3 && \
-  export RABBITMQ_USER=aaa && \
-  export RABBITMQ_PASSWORD=bbb && \
+  export RABBITMQ_USER=user && \
+  export RABBITMQ_PASSWORD=password && \
   export RABBITMQ_ERLANG_COOKIE="secret" && \
   export SUDO="" && \
   make deploy

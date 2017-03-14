90f79a312265f93a7f6e1f2102aec755714a8ae2
Erez Rabih <erez.rabih@gmail.com>
2017-03-14 08:23:21 +0200

    removed deploy script

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export RABBITMQ_REPLICAS=3 && \
-  export RABBITMQ_USER=user && \
-  export RABBITMQ_PASSWORD=password && \
-  export RABBITMQ_ERLANG_COOKIE="secret" && \
-  export SUDO="" && \
-  make deploy

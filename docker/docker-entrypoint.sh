e95130adc705b56a320b8f97e9969c9384c1d6f9
Erez Rabih <erez.rabih@gmail.com>
2017-03-16 14:21:40 +0200

    set ha all on all queues

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -299,6 +299,10 @@ if [ "$haveSslConfig" ] && [ -f "$combinedSsl" ]; then
 	export RABBITMQ_CTL_ERL_ARGS="$RABBITMQ_SERVER_ADDITIONAL_ERL_ARGS"
 fi
 
+exec "$@" &
+
+sleep 10
+
 rabbitmqctl set_policy ha-all ".*" '{"ha-mode":"all"}' --apply-to queues
 
-exec "$@"
+fg

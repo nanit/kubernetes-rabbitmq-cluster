759bad0a3af4e65d5a51c1f0908d3772629fd309
Erez Rabih <erez.rabih@gmail.com>
2018-08-12 13:57:47 +0300

    add expiry policy

--- set_ha.sh
+++ set_ha.sh
@@ -5,6 +5,7 @@ while true ; do
     echo "rabbitmq-0 pod ready ,setting ha policy: $RABBITMQ_HA_POLICY"
     sleep 20
     kubectl exec -n ${NAMESPACE} rabbitmq-0 -- rabbitmqctl set_policy ha-all '.*' "$RABBITMQ_HA_POLICY" --apply-to queues
+    kubectl exec -n ${NAMESPACE} rabbitmq-0 -- rabbitmqctl set_policy expiry '.*' '{"expires":1800000}' --apply-to queues
     echo "ha-all policy set successfully"
     break
   fi

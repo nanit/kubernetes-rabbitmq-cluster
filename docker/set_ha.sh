11f2436e82102e35e5a28471934dc51b12868fb2
yotam <yotam.lender@nanit.com>
2018-09-03 11:34:01 +0300

    fix policies script

--- docker/set_ha.sh
+++ docker/set_ha.sh
@@ -5,8 +5,8 @@ while true ; do
   if [ $(rabbitmqctl status) ]; then
     echo "RabbitMQ is ready, setting ha policy: $RABBITMQ_HA_POLICY"
     sleep 20
-    kubectl exec -n ${NAMESPACE} rabbitmq-0 -- rabbitmqctl set_policy ha-all '.*' "$RABBITMQ_HA_POLICY" --apply-to queues
-    kubectl exec -n ${NAMESPACE} rabbitmq-0 -- rabbitmqctl set_policy expiry '.*' '{"expires":1800000}' --apply-to queues
+    rabbitmqctl set_policy ha-all '.*' "$RABBITMQ_HA_POLICY" --apply-to queues
+    rabbitmqctl set_policy expiry '.*' '{"expires":1800000}' --apply-to queues
     echo "ha-all policy set successfully"
     break
   fi

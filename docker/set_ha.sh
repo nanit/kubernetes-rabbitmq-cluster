ac84ea9181a4d444284c59ff74dc73b64768221f
yotam <yotam.lender@nanit.com>
2018-09-03 12:03:55 +0300

    use hostname in policies script

--- docker/set_ha.sh
+++ docker/set_ha.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -ex
 while true ; do 
   sleep 20
+  hostname=$(hostname -s).rmq-cluster
   echo "Waiting for RabbitMQ be ready...."
-  if [ $(rabbitmqctl status) ]; then
+  if [ $(rabbitmqctl -n ${hostname} status) ]; then
     echo "RabbitMQ is ready, setting ha policy: {{RABBITMQ_HA_POLICY}}"
     sleep 20
-    rabbitmqctl set_policy ha-all '.*' "{{RABBITMQ_HA_POLICY}}" --apply-to queues
-    rabbitmqctl set_policy expiry '.*' '{"expires":1800000}' --apply-to queues
+    rabbitmqctl -n ${hostname} set_policy ha-all '.*' "{{RABBITMQ_HA_POLICY}}" --apply-to queues
+    rabbitmqctl -n ${hostname} set_policy expiry '.*' '{"expires":1800000}' --apply-to queues
     echo "ha-all policy set successfully"
     break
   fi

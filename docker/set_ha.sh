46d767f54530f85a06a8a5c0bb01034a17fcc09d
yotam <yotam.lender@nanit.com>
2018-09-03 16:57:58 +0300

    dummy

--- docker/set_ha.sh
+++ docker/set_ha.sh
@@ -7,7 +7,7 @@ while true ; do
   if [ ${ready} == 0 ]; then
     echo "RabbitMQ is ready, setting ha policy"
     sleep 5
-    rabbitmqctl set_policy ha-all '.*' {{RABBITMQ_HA_POLICY}} --apply-to queues
+    rabbitmqctl set_policy ha-all '.*' '{{RABBITMQ_HA_POLICY}}' --apply-to queues
     rabbitmqctl set_policy expiry '.*' '{"expires":1800000}' --apply-to queues
     echo "ha-all policy set successfully"
     break

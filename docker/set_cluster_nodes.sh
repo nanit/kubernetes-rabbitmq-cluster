319eb50300da02b8220d8f96d55d5994d892d0fe
Erez Rabih <erez.rabih@gmail.com>
2019-06-23 13:06:41 +0300

    deploy on another release name

--- docker/set_cluster_nodes.sh
+++ docker/set_cluster_nodes.sh
@@ -6,7 +6,7 @@ set -u
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
 STATEFUL_SETS=$(curl -f -k https://${KUBERNETES_SERVICE_HOST}/apis/apps/v1beta1/statefulsets -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)")
-RMQ_SS=$(echo $STATEFUL_SETS | jq '.items[] | select(.metadata.name == "rabbitmq-test")')
+RMQ_SS=$(echo $STATEFUL_SETS | jq ".items[] | select(.metadata.name == \"${RABBITMQ_APP_NAME}\")")
 REPLICAS=$(echo $RMQ_SS | jq .spec.replicas)
 SERVICE_NAME=$(echo $RMQ_SS | jq .spec.serviceName | tr -d '"')
 (( REPLICAS-= 1 ))
@@ -14,10 +14,10 @@ NODES=()
 
 for i in $(seq 0 $REPLICAS)
 do
-  NODES+=("{'rabbit@rabbitmq-$i.${SERVICE_NAME}', disc}")
+  NODES+=("{'rabbit@${RABBITMQ_APP_NAME}-$i.${SERVICE_NAME}', disc}")
 done
 
 JOINED=$(join_by , "${NODES[@]}")
 
 sed -i "s/@@RABBITMQ_NODES@@/$JOINED/g" /etc/rabbitmq/clusterer.config
-sed -i "s/@@RABBITMQ_GOSPEL_NODE@@/'rabbit@rabbitmq-0.$SERVICE_NAME'/g" /etc/rabbitmq/clusterer.config
+sed -i "s/@@RABBITMQ_GOSPEL_NODE@@/'rabbit@${RABBITMQ_APP_NAME}-0.$SERVICE_NAME'/g" /etc/rabbitmq/clusterer.config

ed7c91cadf6934a87a9bcd28264dd3f6c4c05213
Erez Rabih <erez.rabih@gmail.com>
2019-06-23 14:28:25 +0300

    added wait for resolvable dns host

--- docker/set_cluster_nodes.sh
+++ docker/set_cluster_nodes.sh
@@ -5,19 +5,19 @@ set -u
 
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
-RABBITMQ_HOSTNAME=${POD_NAME}.${SERVICE_NAME}
-until host ${RABBITMQ_HOSTNAME}
-do
-  echo "Waiting for ${RABBITMQ_HOSTNAME} to be resolved"
-done
-echo ${RABBITMQ_HOSTNAME} resolved!
-
 STATEFUL_SETS=$(curl -f -k https://${KUBERNETES_SERVICE_HOST}/apis/apps/v1beta1/statefulsets -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)")
 RMQ_SS=$(echo $STATEFUL_SETS | jq ".items[] | select(.metadata.name == \"${RABBITMQ_APP_NAME}\")")
 REPLICAS=$(echo $RMQ_SS | jq .spec.replicas)
 SERVICE_NAME=$(echo $RMQ_SS | jq .spec.serviceName | tr -d '"')
 (( REPLICAS-= 1 ))
 NODES=()
+RABBITMQ_HOSTNAME=${POD_NAME}.${SERVICE_NAME}
+
+until host ${RABBITMQ_HOSTNAME}
+do
+  echo "Waiting for ${RABBITMQ_HOSTNAME} to be resolved"
+done
+echo ${RABBITMQ_HOSTNAME} resolved!
 
 for i in $(seq 0 $REPLICAS)
 do
